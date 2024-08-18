04c817827054c38cf40be175aac1c125bfd4b865
Rajkuttyr <118738204+Rajkuttyr@users.noreply.github.com>
2024-08-18 21:25:19 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 create_page = st.Page("stm.py", title="Doctors page", icon=":material/add_circle:")
 Pharma = st.Page("Pharmacy.py", title="Pharmacy", icon=":material/delete:")
-Test=st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\test.py",title='Labouratory')
-result=st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\Test_results.py",title='Test Resuls')
-rad=st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\RandD.py",title='R&D')
+Test=st.Page("test.py",title='Labouratory')
+result=st.Page("Test_results.py",title='Test Resuls')
+rad=st.Page("RandD.py",title='R&D')
 pg = st.navigation([create_page, Pharma,Test,result,rad])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
