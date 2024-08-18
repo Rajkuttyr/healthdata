9695711b1a80a6433ae28c6febd9250035e4dd6c
Rajkuttyr <118738204+Rajkuttyr@users.noreply.github.com>
2024-08-18 21:22:46 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-create_page = st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\stm.py", title="Doctors page", icon=":material/add_circle:")
+create_page = st.Page("stm.py", title="Doctors page", icon=":material/add_circle:")
 Pharma = st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\Pharmacy.py", title="Pharmacy", icon=":material/delete:")
 Test=st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\test.py",title='Labouratory')
 result=st.Page(r"C:\Users\admin\OneDrive\Desktop\streamlit\Test_results.py",title='Test Resuls')
